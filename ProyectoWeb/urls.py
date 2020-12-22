96d67f93dd2037d2b720e265398dce9342a99e5d
CesJauregui <cjaureguisaavedra@gmail.com>
2020-12-21 19:55:58 -0500

    URL's and Templates

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ProyectoWebApp import views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="Home"),
-    path('servicios/', views.servicios, name="Servicios"),
-    path('tienda/', views.tienda, name="Tienda"),
-    path('blog/', views.blog, name="Blog"),
-    path('contacto/', views.contacto, name="Contacto"),
+    path('', include('ProyectoWebApp.urls')),
 ]
